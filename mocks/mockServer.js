8e966015b50e2ba1c387fab5a41c16ff29c7cbec
Jayp11 <jayp11@users.noreply.github.com>
2025-09-04 14:56:18 +0530

    Remove unused files: AdminDashboard, FileUpload, WalletRegister, Register backup, and mockServer

--- mocks/mockServer.js
+++
@@ -1,7 +0,0 @@
-// src/mocks/mockServer.js
-// This file starts the MSW worker in development mode
-
-if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./browser');
-  worker.start();
-}
